404a6ee0696d0f681de6e434105c1bf5978ddf0e
softcoded78 <cataoanmaricel78@gmail.com>
2026-09-20 13:50:53 +0800

    Create 02_string_practice.py

---
+++ 02_string_practice.py
@@ -0,0 +1,5 @@
+# 02_strings_practice.py
+# Description: Practicing basic string methods - upper(), lower(), len(), 
+# isalpha(), slicing [:2], replace(), and escape characters \n \t
+# Author: Icel - BSCS
+# Date: Sept 2026
