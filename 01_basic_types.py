55d23bb4fa73b955762475b31fc52a2a4448da23
softcoded78 <cataoanmaricel78@gmail.com>
2026-09-20 13:44:51 +0800

    Create 01_basic_types.py

---
+++ 01_basic_types.py
@@ -0,0 +1,13 @@
+name = 'Icel'
+age = 19
+
+print(name) # Icel
+print(age) # 19
+
+print(name, type(name)) # Icel <class 'str'>
+print(age, type(age)) # 19 <class 'int'>
+
+print(isinstance(name, char)) # False
+print(isinstance(name, str)) # True
+print(isinstance(age, float)) # False
+print(isinstance(age, int)) # True
