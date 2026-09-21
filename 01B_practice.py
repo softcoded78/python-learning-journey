fe3677674205e9cbe5d97fa12505b199fca41c1d
softcoded78 <cataoanmaricel78@gmail.com>
2026-09-21 11:28:51 +0800

    report card printer
    learning how to build a report card printer

---
+++ 01B_practice.py
@@ -0,0 +1,25 @@
+name = 'Alice'
+print(name) # Alice
+print(type(name)) # <class 'str'> 
+print(name, type(name)) # Alice <class 'str'>  
+
+is_student = True
+print(is_student, type(is_student)) # True <class 'bool'>
+# type is used to know the data type of a variable
+
+age = 20
+print(age, type(age)) # 20 <class 'int'> 
+
+score=80.5
+print(isinstance(score,int)) # False
+# isinstance is used to check the type of a variable
+
+print (score, type(score)) # 80.5 <class 'float'> 
+
+# Alice
+# <class 'str'>
+# Alice <class 'str'>
+# True <class 'bool'>
+# 20 <class 'int'>
+# False
+# 80.5 <class 'float'>
